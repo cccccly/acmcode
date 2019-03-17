234a5f6ffbb68d23acf7855c43c4614efa77b674
cccccly <919345318@qq.com>
2019-03-18 00:05:36 +0800

    update

---
+++ 练习/简单搜索/G.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#define LOCAL
+using namespace std;
+char s1[105],s2[105],temp[210],dest[210],first1[105],first2[105];
+int N,C,pd,cnt;
+
+bool judge(){
+	if(cnt > 1 && strcmp(first1,s1)==0 && strcmp(first2,s2)==0){
+		cnt = -1;
+		return 1;
+	}
+	int res = strncmp(dest,temp,2*C);
+	if(res == 0)
+		return 1;
+	else return 0;
+}
+
+void shuffle(){
+	pd = 0;
+	for(int i = 0;i < C;i++){
+		temp[pd++] = s2[i];
+		temp[pd++] = s1[i];
+	}
+	temp[2*C] = '\0';
+
+	//printf("%s\n",first);
+	strncpy(s2,temp,C);
+	strncpy(s1,temp+C,C);
+	s1[C] = '\0';
+	s2[C] = '\0';
+	//printf("%s\n%s\n",s1,s2);
+}
+
+int main(){
+	#ifdef LOCAL
+		freopen("data\\G.in","r",stdin);
+	#endif
+	scanf("%d",&N);
+	for(int i = 1;i <= N;i++){
+		cnt = 0;
+		scanf("%d %s %s %s",&C,s1,s2,dest);
+		strcpy(first1,s1);
+		strcpy(first2,s2);
+		do{
+			shuffle();
+			cnt++;
+		}while(!judge());
+		if(cnt == -1)
+			printf("%d -1\n",i);
+		else printf("%d %d\n",i,cnt);
+	}
+	return 0;
+} 
