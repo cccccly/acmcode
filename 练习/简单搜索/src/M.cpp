3f38b97f6747ff6e348f1e623e144fa2e3743118
cccccly <919345318@qq.com>
2019-04-18 22:37:16 +0800

    1

--- 练习/简单搜索/src/M.cpp
+++ 练习/简单搜索/src/M.cpp
@@ -1,48 +1,84 @@
 #include <stdio.h>
 #include <queue>
-
-uisng namespace std;
+#include <string.h>
+using namespace std;
 
 struct node{
-	int c[3];
+	int c[3],step;
 };
 
-int S,N,M;
+int C[3];
+bool vis[101][101][101];
 int cnt = 0;
 queue <node> q;
 
-node pour(int x,int y){
-	node next;
-	for(int i = 0;i < 3;i++)
+void printN(node tmp){
+	printf("%d %d %d %d\n",tmp.c[0],tmp.c[1],tmp.c[2],tmp.step);
 }
 
-void bfs(){
+
+
+int bfs(){
 	node cur,next;
 	while(!q.empty()){
+		cur = q.front();
+		//printN(cur);
+		q.pop();
 		for(int i = 0;i < 3;i++){
 			for(int j = 1;j <= 2;j++){
-				if(next.s)
+				next = cur;
+				int k = (i+j)%3;
+				if(cur.c[i] + cur.c[k] > C[k]){
+					next.c[k] = C[k];
+					next.c[i] = cur.c[i] + cur.c[k] - C[k];
+				}
+				else{
+					next.c[k] = cur.c[i] + cur.c[k];
+					next.c[i] = 0;
+				}
+				if(vis[next.c[0]][next.c[1]][next.c[2]])
+					continue;
+				vis[next.c[0]][next.c[1]][next.c[2]] = 1;
+				next.step++;
+				//printN(next);
+				if(next.c[i] == C[0]/2){
+					if(next.c[(i+1)%3] == C[0]/2 || next.c[(i+2)%3] == C[0]/2)
+						return next.step;
+					else return next.step+1;
+				}
+				else if(next.c[k] == C[0]/2){
+					if(next.c[(k+1)%3] == C[0]/2 || next.c[(k+2)%3] == C[0]/2)
+						return next.step;
+					else return next.step+1;
+				}	
+				q.push(next);
 			}
 		}
 	}
-	
+	return 0;
 }
 
 int main(){
 	freopen("..//data//m.in","r",stdin);
-    while(~scanf("%d %d %d",&S,&N,&M) && (S||N||M)){
+    while(~scanf("%d %d %d",&C[0],&C[1],&C[2]) && (C[0]||C[1]||C[2])){
         //printf("%d %d %d\n",S,N,M);
-        if(S%2){
+        if(C[0]%2){
         	puts("NO");
         	continue;
 		}
 		while(!q.empty()) q.pop();
 		cnt = 0;
+		memset(vis,0,sizeof(vis));
 		node tmp;
-		tmp.s[0] = S;
-		tmp.s[1] = tmp.s[2] = 0;
+		tmp.c[0] = C[0];
+		tmp.c[1] = tmp.c[2] = 0;
+		vis[tmp.c[0]][tmp.c[1]][tmp.c[2]] = 1;
+		tmp.step = 0;
 		q.push(tmp);
-		bfs();
+		int ans = bfs();
+		if(ans!=0)
+			printf("%d\n",ans);
+		else puts("NO");
     }
     return 0;
 }
---
+++ 练习/简单搜索/src/N.cpp
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <queue>
+#include <string.h>
+using namespace std;
+
+struct node{
+	int x,y,step;
+};
+
+int n,m;
+char maze[210][210];
+bool vis1[210][210],vis2[210][210];
+int dx[4] = {0,0,-1,1};
+int dy[4] = {1,-1,0,0};
+node ans[210][210];
+queue <node> q1,q2;
+
+
+void init(){
+	for(int i = 0;i < n;i++){
+		scanf("%s",maze[i]);
+	}
+	while(!q1.empty()) q1.pop();
+	while(!q2.empty()) q2.pop();
+	memset(vis1,0,sizeof(vis1));
+	memset(vis2,0,sizeof(vis2));
+	for(int i = 0;i < n;i++){
+		for(int j = 0;j < m;j++){
+			char tmp = maze[i][j];
+			if(tmp == 'Y'){
+				q1.push(node{i,j,0});
+			}
+			else if(tmp == 'M'){
+				q2.push(node{i,j,0});
+			}
+			else if(tmp == '@'){
+				ans[i][j].x = ans[i][j].y = -1;
+			}
+		}
+	}
+}
+
+bool judge1(node tmp){
+	int x = tmp.x,y = tmp.y;
+	if(x >= 0 && x < n && y >= 0 && y<m &&!vis1[x][y] && maze[x][y] != '#')
+		return 1;
+	else return 0; 
+}
+
+bool judge2(node tmp){
+	int x = tmp.x,y = tmp.y;
+	if(x >= 0 && x < n && y >= 0 && y<m &&!vis2[x][y] && maze[x][y] != '#')
+		return 1;
+	else return 0; 
+}
+void bfs1(){
+	node cur,next;
+	while(!q1.empty()){
+		cur = q1.front();
+		q1.pop();
+		for(int i = 0;i < 4;i++){
+			next.x = cur.x + dx[i];
+			next.y = cur.y + dy[i];
+			if(!judge1(next))
+				continue;
+			next.step = cur.step + 1;
+			if(maze[next.x][next.y] == '@')
+				ans[next.x][next.y].x = next.step;
+			q1.push(next);
+		}
+	} 
+}
+
+void bfs2(){
+	node cur,next;
+	while(!q2.empty()){
+		cur = q2.front();
+		q2.pop();
+		for(int i = 0;i < 4;i++){
+			next.x = cur.x + dx[i];
+			next.y = cur.y + dy[i];
+			if(!judge2(next))
+				continue;
+			next.step = cur.step + 1;
+			if(maze[next.x][next.y] == '@')
+				ans[next.x][next.y].y = next.step;
+			q2.push(next);
+		}
+	} 
+}
+
+void solve(){
+	int min = 50010;
+	for(int i = 0;i < n;i++){
+		for(int j = 0;j < m;j++){
+			if(maze[i][j] == '@' && ans[i][j].x != -1 && ans[i][j].x == ans[i][j].y && ans[i][j].x < min){
+				min = ans[i][j].x;
+			}
+		}
+	}
+	printf("%d\n",min);
+}
+
+
+int main(){
+	freopen("..//data//n.in","r",stdin);
+	while(~scanf("%d %d",&n,&m)){
+		bfs1();
+		bfs2();
+		solve();
+	}
+	return 0;
+}
